8b0119c9c790347299fffb41a143415bfb526bbd
brycealbertazzi <yourgitemail@example.com>
2021-02-14 11:13:21 -0800

    Title Created

---
+++ src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <div className="bg-dark py-3">
+            <h1 className="text-light">Bryce Albertazzi</h1>
+        </div>
+    )
+}
