3ca1c0bc9d3ae31a7c7bb403895fafd84276125b
brycealbertazzi <yourgitemail@example.com>
2021-02-14 20:58:32 -0800

    Minor Update

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ export const About = () => {
                 &emsp; &emsp; Hello my name is Bryce Albertazzi. I am graduating from Oregon State in 2021 with a computer science degree. I am passionate about developing software that solves
                 problems and makes people's lives easier. I have a wide variey of skills in mobile/web/cloud development and deployment. I discovered computer science was a great fit
                 for me when I began making games with Unity and publishing some of my games onto the iOS App Store and other platforms. Later, I got into web and mobile developmet.
-                I learned the ropes of several different web/moblile dev platforms including Angular, React, React Native, Ionic, Vue, Flutter, XCode and Andriod Studio. I then worked
+                I learned the ropes of several different web/moblile dev frameworks including Angular, React, React Native, Ionic, Vue, Flutter, XCode and Andriod Studio. I then worked
                 to expand my skills further by exploring cloud development. While doing this I earned AWS Cloud Practionier and AWS Developer certifications. I also familiarized myself 
                 with deploying to platforms such as Google Firebase, Netlify, AWS, and more. I believe I can be an asset to many companies with my broad experience in many
                 areas of computer science.
