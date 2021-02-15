7b44c71bd5ff4cbfe2ecf751e6378e01eaf4e178
brycealbertazzi <yourgitemail@example.com>
2021-02-14 18:33:30 -0800

    Removed Place FInder

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,6 @@ export const ProjectList = () => {
         <div className="bg-secondary py-5">
             {/* List all project items below with a title and a link */}
                 <ProjectItem title="Client Panel (Angular)" link="https://clientpanelprod-23957.firebaseapp.com"/>
-                <ProjectItem title="Place Finder (Angular + Ionic)" link="https://place-finder-67a19.firebaseapp.com"/>
                 <ProjectItem title="Note Keeper (Angular + Ionic)" link="https://note-keeper-3e377.firebaseapp.com/collections"/>
                 <ProjectItem title="Github Finder (React)" link="https://githubfinder0820.netlify.app"/>
                 <ProjectItem title="Starwars Zombies (Unity Game)" link="https://gamejolt.com/games/starwars-zombies/421504"/>
