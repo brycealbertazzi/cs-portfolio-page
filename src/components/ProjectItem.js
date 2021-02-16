8c8d4a4e6e7ce939fbae8cd78144f2802d78f0dc
brycealbertazzi <yourgitemail@example.com>
2021-02-16 13:40:38 -0800

    small change

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const ProjectItem = ({title, link}) => {
     return (
-        <div className="card text-center text-light m-4 p-3 border border-light" style={{backgroundColor: "var(--gray-900)"}}>
+        <div className="card text-center text-light m-4 p-3 border border-light" style={{backgroundColor: "#212529"}}>
             <h3 className="text-success">{title}</h3>
             {/* If we have a link, display it */}
             {link && <a href={link} target="_blank">View</a>}
